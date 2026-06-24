dae0df316168ba201aa07166511a62f545eef143
Dunghthe04 <tuandungyz09@gmail.com>
2026-06-24 21:52:34 +0700

    Add comment in IAuthService file

--- backend/ToeicMasterPro.Application/Common/Interfaces/IAuthService.cs
+++ backend/ToeicMasterPro.Application/Common/Interfaces/IAuthService.cs
@@ -5,8 +5,13 @@ namespace ToeicMasterPro.Application.Common.Interfaces;
 
 public interface IAuthService
 {
+    //Task = pthuc bat dong bo
+    //Result = chỉ thông báo thành công/ thất bại, ko kèm dữ liệu
+    //Result<T> = thành công kèm dữ liệu T
     Task<Result> RegisterAsync(RegisterRequest req);
+    // Hàm đăng nhập, trả về Token + RefreshToken
     Task<Result<AuthResponse>> LoginAsync(LoginRequest req);
+    // Làm mới token (lấy token mới bằng RefreshToken cũ)
     Task<Result<AuthResponse>> RefreshTokenAsync(string refreshToken);
     Task<Result> LogoutAsync(string refreshToken);
     Task<Result> ConfirmEmailAsync(Guid userId, string token);
